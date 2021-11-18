a0b8e12dead1c354e9d38d7b4b40cc97d2613a5d
Matéo <mateo.esteves@etu.univ-nantes.fr>
2021-11-18 10:25:35 +0100

    v0.02

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -1,5 +1,13 @@
-pubic class Vehicule {
+import java.util.*;
 
+pubic class Vehicule { //superclass vehicule
+
+    protected String name;
+    protected double price;
+    protected double speed;
+    protected double co2;
+    protected double weight;
+    protected double utilityPrice;
 
 
 
---
+++ src/Velo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+pubic class Velo extends Vehicule { //subclass Velo
+
+    private
+
+
+
+
+
+}
