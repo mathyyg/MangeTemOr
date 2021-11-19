b42b8230442f5164cf1bc1d97332b0ad1102f73c
Matéo <mateo.esteves@etu.univ-nantes.fr>
2021-11-19 10:00:00 +0100

    v0.04 ==> ajout constructeur

--- src/Scooter.java
+++ src/Scooter.java
@@ -2,6 +2,24 @@ import java.util.*;
 
 pubic class Scooter extends Vehicule { //subclass Scooter
 
+    private double zyl;
+    private double scooterconso;
+    private double scooterspeed;
+    private double scooterco2;
+    private double scooterusingprice;
+    private double sp98price;
+
+    public Scooter(String s, double p, double w, double cyl, double cons) {
+        this.zyl = cyl;
+        this.scooterconso = cons;
+        this.scooterspeed = 30 + (this.zyl/50);
+        this.scooterco2 = (this.zyl/4);
+        this.sp98price = 1.65;
+        this.scooterusingprice = (p/20000) + (this.scooterconso * this.sp98price);
+        super(s,p,scooterspeed,scooterco2,w,scooterusingprice);
+
+    }
+
 
 
 
--- src/Velo.java
+++ src/Velo.java
@@ -5,7 +5,16 @@ pubic class Velo extends Vehicule { //subclass Velo
     private double avgspeed;
     private double veloco2;
     private double veloweight;
-    private double usingPrice;
+    private double velousingPrice;
+
+    public Velo(String s, double p, double sp, double up) {
+        this.veloco2 = 0;
+        this.veloweight = 0;
+        this.velousingPrice = p/30000;
+        super(s,p,sp,veloco2,veloweight,velousingPrice); // revoir pour la speed car dépend du cycliste
+
+    }
+
 
 
 
