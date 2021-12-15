ef0fd4ef553d24107c2ecaf891ce35ea15520b08
e194188z <mateo.esteves@etu.univ-nantes.fr>
2021-12-15 18:48:59 +0100

    let's go

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-import main.LancementFrame;
+import main.*;
 
 public class Main {
-    public static void main(String[] args) {
-        LancementFrame lancementFrame = new LancementFrame();
+    public static void main(String[] args) throws Exception {
+
+        Entreprise entreprise = new Entreprise();
+        Salaree salaree = new Salaree("La Voisine",55,0,false);
+        entreprise.addSalaree(salaree);
+        Vehicule velo = new Velo("Gitane",500,15);
+        entreprise.addGa(velo);
+        Vehicule scooter = new Scooter("YAMAMA",500,150,125,8);
+        entreprise.addGa(scooter);
+
+
+
+
+
+
+
+
+
+
+
+        LancementFrame lancementFrame = new LancementFrame(entreprise);
         lancementFrame.setVisible(true);
+
     }
 }
