7110e7e03c64d305a7812dd9b1592d44597e8e9a
mathys <mathys.gagner@etu.univ-nantes.fr>
2021-12-17 16:14:42 +0100

    Commit membre du groupe fantome

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -45,7 +45,6 @@ public class MainFrame extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Mateo Esteves
         menuBar1 = new JMenuBar();
         menu1 = new JMenu();
         menuItem1 = new JMenuItem();
@@ -163,13 +162,7 @@ public class MainFrame extends JFrame {
 
             //======== panel3 ========
             {
-                panel3.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax
-                .swing.border.EmptyBorder(0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing
-                .border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.
-                Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt.Color.red
-                ),panel3. getBorder()));panel3. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override
-                public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062or\u0064er".equals(e.getPropertyName(
-                )))throw new RuntimeException();}});
+
                 panel3.setLayout(new BorderLayout());
 
                 //======== panel5 ========
@@ -882,7 +875,6 @@ public class MainFrame extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Mateo Esteves
     private JMenuBar menuBar1;
     private JMenu menu1;
     private JMenuItem menuItem1;
