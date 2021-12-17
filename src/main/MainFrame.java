af8b2cfe68ca2d3ab1661584d1fd2559721bafd3
e194188z <mateo.esteves@etu.univ-nantes.fr>
2021-12-17 16:08:56 +0100

    finito

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -12,7 +12,14 @@ import java.util.EventListener;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.*;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.IntelliJTheme;
+import com.formdev.flatlaf.intellijthemes.*;
+import net.miginfocom.layout.AC;
 import net.miginfocom.swing.*;
 import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.Waypoint;
@@ -26,6 +33,8 @@ public class MainFrame extends JFrame {
     private HashMap<String, Course> courses = new HashMap<>();
     private CoursesPanel coursesPanel = new CoursesPanel();
     private static int countCourses =0;
+    private ThemeFrame themeFrame = new ThemeFrame();
+    private HashMap<String, IntelliJTheme.ThemeLaf> themes = new HashMap<String, IntelliJTheme.ThemeLaf>();
 
     public MainFrame(Entreprise e) {
         this.entreprise = e;
@@ -35,6 +44,7 @@ public class MainFrame extends JFrame {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Mateo Esteves
         menuBar1 = new JMenuBar();
         menu1 = new JMenu();
         menuItem1 = new JMenuItem();
@@ -43,7 +53,8 @@ public class MainFrame extends JFrame {
         menuItem4 = new JMenuItem();
         menuItem5 = new JMenuItem();
         menuItem6 = new JMenuItem();
-        menuItem2 = new JMenuItem();
+        separator2 = new JSeparator();
+        checkBoxMenuItem1 = new JCheckBoxMenuItem();
         tabbedPane1 = new JTabbedPane();
         panel3 = new JPanel();
         panel5 = new JPanel();
@@ -134,11 +145,12 @@ public class MainFrame extends JFrame {
                 }
                 menu2.add(menu3);
                 menu2.addSeparator();
+                menu2.add(separator2);
 
-                //---- menuItem2 ----
-                menuItem2.setText("Liste des th\u00e8mes");
-                menuItem2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, KeyEvent.CTRL_DOWN_MASK));
-                menu2.add(menuItem2);
+                //---- checkBoxMenuItem1 ----
+                checkBoxMenuItem1.setText("Liste des th\u00e8mes");
+                checkBoxMenuItem1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, KeyEvent.CTRL_DOWN_MASK));
+                menu2.add(checkBoxMenuItem1);
             }
             menuBar1.add(menu2);
         }
@@ -149,6 +161,13 @@ public class MainFrame extends JFrame {
 
             //======== panel3 ========
             {
+                panel3.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax
+                .swing.border.EmptyBorder(0,0,0,0), "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e",javax.swing
+                .border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.
+                Font("D\u0069al\u006fg",java.awt.Font.BOLD,12),java.awt.Color.red
+                ),panel3. getBorder()));panel3. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override
+                public void propertyChange(java.beans.PropertyChangeEvent e){if("\u0062or\u0064er".equals(e.getPropertyName(
+                )))throw new RuntimeException();}});
                 panel3.setLayout(new BorderLayout());
 
                 //======== panel5 ========
@@ -423,6 +442,43 @@ public class MainFrame extends JFrame {
         setSize(870, 520);
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        initThemes();
+        DefaultListModel listModelTheme = new DefaultListModel();
+        for(String theme : this.themes.keySet()) {
+            listModelTheme.addElement(theme);
+        }
+        this.themeFrame.jlistThemes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        this.themeFrame.jlistThemes.setModel(listModelTheme);
+        this.themeFrame.jlistThemes.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                String res = "Arc";
+                if(!e.getValueIsAdjusting()) {
+                    res = themeFrame.jlistThemes.getSelectedValue().toString();
+                }
+                updateLF(res);
+            }
+        });
+        this.themeFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                checkBoxMenuItem1.setSelected(false);
+                e.getWindow().dispose();
+            }
+        });
+        checkBoxMenuItem1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(checkBoxMenuItem1.isSelected()) {
+//                    scrollPane1.setVisible(true);
+                    themeFrame.setVisible(true);
+                }
+                else {
+//                    scrollPane1.setVisible(false);
+                    themeFrame.setVisible(false);
+                }
+            }
+        });
 
         this.panel3.add(coursesPanel, BorderLayout.CENTER);
 
@@ -439,42 +495,183 @@ public class MainFrame extends JFrame {
 
         button1.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) { // ajout de velo via interface
                 DefaultTableModel model = (DefaultTableModel) table1.getModel();
+                if(!textField1.getText().equals("")&&!textField2.getText().equals("")) {
+                    Vehicule newvehicule = new Velo(textField1.getText(),Double.valueOf(textField2.getText()),Double.valueOf(textField2.getText())/30000);
+                    entreprise.addGa(newvehicule);
+                    model.insertRow(0,new Object[]{ textField1.getText(), textField2.getText(), Double.valueOf(textField2.getText())/30000});
+                    initComboEmploye();
+                }
+                else {
+                    JOptionPane.showMessageDialog(getParent(), "Les Champs sont vide stop faire exprès", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
 
-                model.insertRow(0,new Object[]{ textField1.getText(), textField2.getText(), Double.valueOf(textField2.getText())/30000});
             }
         });
 
         button3.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) { // ajout de scooter via l'interface
                 DefaultTableModel modelle = (DefaultTableModel) table2.getModel();
-                modelle.insertRow(0,new Object[]{ textField10.getText(), textField9.getText(), textField8.getText(),textField12.getText(),textField11.getText(),
-                        (Double.valueOf(textField9.getText())/20000) + (Vehicule.getSp98price()*Double.valueOf(textField11.getText())/100)});
+                if (!textField10.getText().equals("")&&!textField9.getText().equals("")&&!textField8.getText().equals("")&&!textField12.getText().equals("")&&!textField11.getText().equals("")) {
+                    Vehicule newvehicule = new Scooter(textField10.getText(),Double.valueOf(textField9.getText()),Double.valueOf(textField8.getText()),Double.valueOf(textField12.getText()),
+                            (Double.valueOf(textField9.getText())/20000) + (Vehicule.getSp98price()*Double.valueOf(textField11.getText())/100));
+                    entreprise.addGa(newvehicule);
+                    modelle.insertRow(0,new Object[]{ textField10.getText(), textField9.getText(), textField8.getText(),textField12.getText(),textField11.getText(),
+                            (Double.valueOf(textField9.getText())/20000) + (Vehicule.getSp98price()*Double.valueOf(textField11.getText())/100)});
+                    initComboVehicule();
+                }
+                else {
+                    JOptionPane.showMessageDialog(getParent(), "Les Champs sont vide stop faire exprès", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+
             }
         });
-        table1.setAutoCreateRowSorter(true);
-        DefaultTableModel model = (DefaultTableModel) table1.getModel();
+        table1.setAutoCreateRowSorter(true); //trieur
+        table1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); //permet d'avoir une selection unique dans le tableau
+        DefaultTableModel model = (DefaultTableModel) table1.getModel(); //tableau velo
 
-        table2.setAutoCreateRowSorter(true);
-        DefaultTableModel modelle = (DefaultTableModel) table2.getModel();
+        table2.setAutoCreateRowSorter(true); //trieur
+        table2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);//permet d'avoir une selection unique dans le tableau
+        DefaultTableModel modelle = (DefaultTableModel) table2.getModel();//tableau scooter
 
         for(Vehicule vehicule : entreprise.getGa().getGarage().keySet() ) {
-            if(vehicule.getClass() == Velo.class) {
+            if(vehicule.getClass() == Velo.class) { //affichage velo
                 model.insertRow(0,new Object[]{ vehicule.getName(), vehicule.price, vehicule.getUsingPrice()});
             }
-            else {
+            else { //affichage scooter
                 modelle.insertRow(0,new Object[]{vehicule.getName(),vehicule.getPrice(),vehicule.getZylindre(),vehicule.getWeight(),vehicule.getConso(),vehicule.getUsingPrice()});
             }
 
         }
 
-        DefaultListModel listModel = new DefaultListModel();
-        for(Salaree salaree : entreprise.getEmploye()) {
+
+
+
+
+        DefaultListModel listModel = new DefaultListModel(); // liste salaree
+        list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); //permet d'avoir une selection unique des salaree dans la liste
+
+
+        for(Salaree salaree : entreprise.getEmploye()) { //affichage salaree saisi dans main
             listModel.addElement(salaree.getName());
         }
-        list1.setModel(listModel);
+
+        list1.addListSelectionListener(new ListSelectionListener() { // affichage infos completes du salaree quand on le selectionne
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if(!e.getValueIsAdjusting()) {
+                    for(Salaree salaree : entreprise.getEmploye()) { //debug
+                        System.out.println(salaree.toString());
+                    }
+                    String s = list1.getSelectedValue().toString();
+                    for(Salaree salaree : entreprise.getEmploye()) {
+                        if(s.equals(salaree.getName())) {
+                            textField5.setText(salaree.getName());
+                            textField6.setText(String.valueOf(salaree.getWeight()));
+                            textField3.setText(String.valueOf(salaree.getSalaire()));
+                            textField4.setText(salaree.getCylcistType().toString());
+                            break;
+                        }
+                    }
+
+                }
+            }
+        });
+
+        list1.setModel(listModel); //liste des salaree
+
+       /* Ne fonctionne pas
+       button7.addActionListener(new ActionListener() { // bouton suppresion salaree
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = listModel.getElementAt(list1.getSelectedIndex()).toString();
+                for(Salaree salaree : entreprise.getEmploye()) {
+                    if(s.equals(salaree.getName())) {
+                        listModel.remove(list1.getSelectedIndex());
+                        entreprise.getEmploye().remove(salaree);
+                        list1.setSelectedIndex(0);
+                    }
+                }
+            }
+        });*/
+
+        button5.addActionListener(new ActionListener() { // bouton sauvegarder les modifs apporter à un salaree
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = list1.getSelectedValue().toString();
+                for(Salaree salaree : entreprise.getEmploye()) {
+                    if(s.equals(salaree.getName()) ) {
+                        salaree.setName(textField5.getText());
+                        salaree.setWeight(Double.valueOf(textField6.getText()));
+                        salaree.setSalaire(Double.valueOf(textField3.getText()));
+                        salaree.setCylcistType(TypeOfCyclist.valueOf(textField4.getText()));
+                        listModel.setElementAt(textField5.getText(),list1.getSelectedIndex());
+                        break;
+                    }
+                }
+            }
+        });
+
+        button6.addActionListener(new ActionListener() { //ajout des salaree a l'arraylist
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!textField5.getText().equals("")  && !textField6.getText().equals("") && !textField3.getText().equals("") && !textField4.getText().equals("")) {
+                    if (textField4.getText().equals("NON")) {
+                        try {
+                            Salaree newsalaree = new Salaree(textField5.getText(), Double.valueOf(textField6.getText()), Double.valueOf(textField3.getText()), false, TypeOfCyclist.NON);
+                            entreprise.addSalaree(newsalaree);
+                            listModel.addElement(newsalaree.getName());
+                            initComboEmploye();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    else {
+                        try {
+                            Salaree newsalaree = new Salaree(textField5.getText(), Double.valueOf(textField6.getText()), Double.valueOf(textField3.getText()),true,TypeOfCyclist.valueOf(textField4.getText()));
+                            entreprise.addSalaree(newsalaree);
+                            listModel.addElement(newsalaree.getName());
+                            initComboEmploye();
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+                else {
+                    JOptionPane.showMessageDialog(getParent(), "Les Champs sont vide stop faire exprès", "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        menuItem1.addActionListener(new ActionListener() {//menu item pour quitter l'appli
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        menuItem4.addActionListener(new ActionListener() { //menuitem vers salarie
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(2);
+            }
+        });
+
+        menuItem5.addActionListener(new ActionListener() { //menuitem vers garage
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(1);
+            }
+        });
+
+        menuItem6.addActionListener(new ActionListener() { // menu item pour lancer une course
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabbedPane1.setSelectedIndex(0);
+            }
+        });
 
         coursesPanel.newCoursePanel1.buttonNewCourse.addActionListener(new ActionListener() {
             @Override
@@ -558,6 +755,49 @@ public class MainFrame extends JFrame {
         });
     }
 
+    private void initThemes() {
+        this.themes.put("Arc", new FlatArcIJTheme());
+        this.themes.put("Arc Orange", new FlatArcOrangeIJTheme());
+        this.themes.put("Arc Dark", new FlatArcDarkIJTheme());
+        this.themes.put("Arc Dark Orange", new FlatArcDarkOrangeIJTheme());
+        this.themes.put("Carbon", new FlatCarbonIJTheme());
+        this.themes.put("Cobalt 2", new FlatCobalt2IJTheme());
+        this.themes.put("Cyan Light", new FlatCyanLightIJTheme());
+        this.themes.put("Dark Flat", new FlatDarkFlatIJTheme());
+        this.themes.put("Dark Purple", new FlatDarkPurpleIJTheme());
+        this.themes.put("Dracula", new FlatDraculaIJTheme());
+        this.themes.put("Gradianto Dark Fuchsia", new FlatGradiantoDarkFuchsiaIJTheme());
+        this.themes.put("Grandianto Deep Ocean", new FlatGradiantoDeepOceanIJTheme());
+        this.themes.put("Gradianto Midnight Blue", new FlatGradiantoMidnightBlueIJTheme());
+        this.themes.put("Gradianto Nature Green", new FlatGradiantoNatureGreenIJTheme());
+        this.themes.put("Gray", new FlatGrayIJTheme());
+        this.themes.put("Gruvbox Dark Hard", new FlatGruvboxDarkHardIJTheme());
+        this.themes.put("Gruvbox Dark Medium", new FlatGruvboxDarkMediumIJTheme());
+        this.themes.put("Gruvbox Dark Soft", new FlatGruvboxDarkSoftIJTheme());
+        this.themes.put("Hiberbee Dark", new FlatHiberbeeDarkIJTheme());
+        this.themes.put("High Contrast", new FlatHighContrastIJTheme());
+        this.themes.put("Light Flat", new FlatLightFlatIJTheme());
+        this.themes.put("Material Design Dark", new FlatMaterialDesignDarkIJTheme());
+        this.themes.put("Monocai", new FlatMonocaiIJTheme());
+        this.themes.put("Nord", new FlatNordIJTheme());
+        this.themes.put("One Dark", new FlatOneDarkIJTheme());
+        this.themes.put("Solarized Dark", new FlatSolarizedDarkIJTheme());
+        this.themes.put("Solarized Light", new FlatSolarizedLightIJTheme());
+        this.themes.put("Spacegray", new FlatSpacegrayIJTheme());
+        this.themes.put("Vuesion", new FlatVuesionIJTheme());
+    }
+
+    public void updateLF(String lf) {
+        try {
+            UIManager.setLookAndFeel(themes.get(lf));
+            FlatLaf.updateUI();
+
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private void initListeCourses() {
         DefaultListModel listModel = new DefaultListModel();
         for(String cid : this.courses.keySet()) {
@@ -601,6 +841,7 @@ public class MainFrame extends JFrame {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Mateo Esteves
     private JMenuBar menuBar1;
     private JMenu menu1;
     private JMenuItem menuItem1;
@@ -609,7 +850,8 @@ public class MainFrame extends JFrame {
     private JMenuItem menuItem4;
     private JMenuItem menuItem5;
     private JMenuItem menuItem6;
-    private JMenuItem menuItem2;
+    private JSeparator separator2;
+    private JCheckBoxMenuItem checkBoxMenuItem1;
     private JTabbedPane tabbedPane1;
     private JPanel panel3;
     private JPanel panel5;
